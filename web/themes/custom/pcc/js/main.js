3df507c0341ad6452a5349323133aac165870682
Oliver Read <oliver@reads.org.uk>
2025-02-26 16:32:53 +0000

    Add section toggling

--- web/themes/custom/pcc/js/main.js
+++ web/themes/custom/pcc/js/main.js
@@ -11,3 +11,35 @@ function on_dark_mode_toggle_change(event) {
     document.documentElement.setAttribute("data-theme", "light");
   }
 }
+
+/**
+ * Code to toggle element visibility for services menu and search bar.
+ */
+
+// Get all the toggle button elements as an array.
+const toggleButtons = Array.from(
+  document.getElementsByClassName("toggle-button"),
+);
+
+// Loop through toggle buttons.
+for (const button of toggleButtons) {
+  // Get the element to show/hide.
+  const target = document.getElementById(button.dataset.target);
+
+  if (target) {
+    target.classList.add("toggle-target");
+
+    // Assign the click event.
+    button.onclick = () => {
+      // Hide all target elements that aren't this one.
+      for (const t of document.getElementsByClassName("toggle-target")) {
+        if (t !== target) {
+          t.classList.remove("active");
+        }
+      }
+
+      // Toggle the active CSS class on the target.
+      target.classList.toggle("active");
+    };
+  }
+}
