b529c5c47e6ddf9fca584fc21c77c84771988942
Oliver Read <ojread@users.noreply.github.com>
2026-04-17 13:23:52 +0100

    260 migrate media (#263)
    * Add media migrations
    
    * Updage packages
    
    * Lint fixes

--- web/themes/custom/pcc/js/subsite-navigation.js
+++ web/themes/custom/pcc/js/subsite-navigation.js
@@ -1,9 +1,9 @@
-document.querySelectorAll('.menu-toggle').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const container = btn.closest('.menu-item');
-    const children = container.querySelector('.menu-children');
+document.querySelectorAll(".menu-toggle").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const container = btn.closest(".menu-item");
+    const children = container.querySelector(".menu-children");
 
-    const isOpen = children.classList.toggle('is-open');
-    btn.setAttribute('aria-expanded', isOpen);
+    const isOpen = children.classList.toggle("is-open");
+    btn.setAttribute("aria-expanded", isOpen);
   });
 });
